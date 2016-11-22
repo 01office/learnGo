c29965b64f940321a4c1e76d4821359b3c36d43b
Neo He <neohe2007@gmail.com>
2016-11-23 00:16:10 +0800

    bubble sort

--- hello.go
+++ hello.go
@@ -4,5 +4,27 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hi, everyone!")
+
+	var i, j, tmp int
+	var a [10]int
+	fmt.Println("Please enter 10 numbers: ")
+	for i = 0; i < 10; i++ {
+		fmt.Scanln(&a[i])
+	}
+
+	for i = 0; i < 10; i++ {
+		for j = 1; j < 10 - i; j++ {
+			if a[j - 1] > a[j] {
+				tmp = a[j]
+				a[j] = a[j - 1]
+				a[j - 1] = tmp
+			}
+		}
+	}
+
+	fmt.Println("After sorted:")
+	for i = 0; i < 10; i++ {
+		fmt.Println(a[i], " ")
+	}
 }
 
