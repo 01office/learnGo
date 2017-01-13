eb9c213d85a9bcbf9b287cb7a112dec5622954d3
Neo He <neohe2007@gmail.com>
2017-01-14 00:11:18 +0800

    OOP

--- lets.go
+++ lets.go
@@ -2,7 +2,16 @@ package letsgo
 
 import (
 	"fmt"
+	"math"
 )
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) erea() float64 {
+	return c.radius * c.radius * math.Pi
+}
 
 func GetMax(a, b int) int {
 	if a > b {
